eae761a25830b8e6e1c1cb4601664c864f0dbce4
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-11-29 16:41:24 +0100

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,22 @@
+/**
+*
+*	Ejercicio 4,  Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+* 
+*	Nombre del archivo: Ejercicio4.java
+*
+*	@author Jorge Reina Romero
+*
+*/
+
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		
+		//Definimos el bucle for.
+		for (int i = 320; i >=160; i = i-20) {
+			
+			System.out.println(i);
+			
+		}
+		
+	}
+}
