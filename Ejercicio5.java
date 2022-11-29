40f2752282c384d34eccf0e096d40d0952a65dd1
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-11-29 16:48:18 +0100

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,25 @@
+/**
+*
+*	Ejercicio 5,  Muestra los números del 320 al 160, contando de 20 en 20
+* hacia atrás utilizando un bucle while.
+* 
+*	Nombre del archivo: Ejercicio5.java
+*
+*	@author Jorge Reina Romero
+*
+*/
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		
+		//Definimos la variable i.
+		int i = 320;
+		
+		//Definimos el bucle while.
+		while (i > 140) {
+			System.out.println(i);
+			i = i - 20;
+		}
+		
+	}
+}
